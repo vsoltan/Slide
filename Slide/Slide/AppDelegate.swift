9202d174392e8f932bb84bd42631cfef52f0af6d
Tyler Clift <tyjclift@gmail.com>
2019-06-04 23:24:29 -0400

    Test: making changes to branch Tyler

--- Slide/Slide/AppDelegate.swift
+++ Slide/Slide/AppDelegate.swift
@@ -12,6 +12,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    //TESTING BRANCH STUFF
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
