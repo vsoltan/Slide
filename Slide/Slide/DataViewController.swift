db05a82e09c49fd8acf450e35e75b5366041ae6d
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-06-04 22:31:50 -0400

    initialized xcode proj

---
+++ Slide/Slide/DataViewController.swift
@@ -0,0 +1,28 @@
+//
+//  DataViewController.swift
+//  Slide
+//
+//  Created by Valeriy Soltan on 6/4/19.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: String = ""
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.dataLabel!.text = dataObject
+    }
+
+
+}
+
