e53a6ca5b773deb03f2511b9efd49fea1aac225f
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-06-06 20:30:16 -0400

    organizational changes

---
+++ Slide/Slide/ViewControllers/LoginViewController.swift
@@ -0,0 +1,18 @@
+//
+//  LoginViewController.swift
+//  Slide
+//
+//  Created by Valeriy Soltan on 6/6/19.
+//  Copyright © 2019 Roodac. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
