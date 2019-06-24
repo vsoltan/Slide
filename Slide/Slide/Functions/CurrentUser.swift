c85856f897bb688382eac27ac4ce958b24538f70
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-06-24 14:22:05 -0400

    got asynchronous calls to work. Imperfect solution, takes a second for the data to load but the data is available for us to use

--- Slide/Slide/Functions/CurrentUser.swift
+++ Slide/Slide/Functions/CurrentUser.swift
@@ -1,5 +1,5 @@
 //
-//  profile.swift
+//  CurrentUser.swift
 //  Slide
 //
 //  Created by Valeriy Soltan on 6/12/19.
@@ -7,39 +7,48 @@
 //
 
 import Foundation
-import FirebaseAuth
-import FirebaseFirestore
+import Firebase
 
 // an interface between the database and the user
 class CurrentUser {
     
+    // the user ID of the currently signed in user
     static var user = Auth.auth().currentUser!.uid
     
     // retrieves the data tree belonging to the current user
-    static func getDocument(completion: @escaping ([QueryDocumentSnapshot]?) -> Void) {
-        // reference to the cloud database
-        let db = Firestore.firestore()
+    static func getDocument(completionHandler: @escaping ([QueryDocumentSnapshot]?) -> Void) {
         
+        // reference to the database
+        let db = Firestore.firestore()
+
+        // asynchronous call to the database
         db.collection("users").whereField("ID", isEqualTo: user).getDocuments { (snapshot, error) in
             if error != nil {
                 // user was not found
                 SlideError.inputError(errorTitle: "Data Retrieval", errorMessage: error!.localizedDescription).show()
-                completion(nil)
+                completionHandler(nil)
             } else {
-                // returns data
-                completion((snapshot?.documents)!)
+                // upon completion, returns a reference to the document
+                completionHandler((snapshot?.documents)!)
             }
         }
     }
     
-    static func getName() -> String {
-        // explicit String return type because error will be thrown in getDocument method
+    // parses through the user's doc tree and finds their name
+    static func getName(completion: @escaping (String?) -> Void) {
+        // thread deployed to interact with database
         self.getDocument() { (userData) in
-            for doc in userData! {
-//                TODO name = doc.data()["Name"] as? String
+            // iterates through the array, as there may be several docs
+            for document in userData! {
+                if let nameData = document.data()["Name"] as? String {
+                    // waits for the thread to complete and returns
+                    completion(nameData)
+                }
             }
         }
-        return "placeholderString"
     }
-
+    /*  NOTE: if we put a return statement here, it would execute before most of the
+        code after the call to the getDocument method, therefore, nameData wouldn't get
+        initialized...which is why we kept getting nil
+    */
 }
--- Slide/Slide/ViewControllers/HomeViewController.swift
+++ Slide/Slide/ViewControllers/HomeViewController.swift
@@ -13,9 +13,16 @@ class HomeViewController: UIViewController {
     @IBOutlet weak var NameLabel: UILabel!
     
     override func viewDidLoad() {
+        
+        // sets the label to the current user's name
+        CurrentUser.getName { (name) in
+            self.NameLabel.text = name
+        }
+        
+        // TODO: look into the delay with the name pop-in
         super.viewDidLoad()
-        NameLabel.attributedText = NSAttributedString(string: "HELLO WORLD", attributes: [NSAttributedString.Key.foregroundColor: UIColor.red])
-        print(CurrentUser.getName())
+        
+        
     }
 }
 
