e597cccdfefb19be199019862059bf3c3b172378
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-07-01 19:41:20 -0400

    resolved issue of qrgenerator creating a code for no data and tweaked the segue animations

--- Slide/Slide/Segues/SwipeDown.swift
+++ Slide/Slide/Segues/SwipeDown.swift
@@ -1,5 +1,5 @@
 //
-//  Swipe.swift
+//  SwipeDown.swift
 //  Slide
 //
 //  Created by Valeriy Soltan on 7/1/19.
@@ -8,18 +8,18 @@
 
 import UIKit
 
-class SwipeLeft: UIStoryboardSegue {
+class SwipeDown: UIStoryboardSegue {
     override func perform() {
         let src = self.source
         let dst = self.destination
         
         // superimpose the destination view controller over the source
         src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
-//        dst.view.transform = CGAffineTransform(translationX: -src.view.frame.size.width, y: 0)
-        dst.view.transform = CGAffineTransform(translationX: -src.view.frame.size.width, y: 0)
-        UIView.animate(withDuration: 0.25,
-            delay: 0.0, options: .curveEaseInOut, animations: {
-                dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
+        dst.view.transform = CGAffineTransform(translationX: 0, y: -src.view.frame.size.width * 2)
+        
+        UIView.animate(withDuration: 0.35,
+                       delay: 0.0, options: .curveEaseInOut, animations: {
+                        dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
         }, completion: { finished in
             src.present(dst, animated: false, completion: nil)})
     }
--- Slide/Slide/Segues/SwipeRight.swift
+++ Slide/Slide/Segues/SwipeRight.swift
@@ -16,7 +16,7 @@ class SwipeRight: UIStoryboardSegue {
         // superimpose the destination view controller over the source
         src.view.superview?.insertSubview(dst.view, aboveSubview: src.view)
         dst.view.transform = CGAffineTransform(translationX: +src.view.frame.size.width, y: 0)
-        
+          
         UIView.animate(withDuration: 0.25,
             delay: 0.0, options: .curveEaseInOut, animations: {
                 dst.view.transform = CGAffineTransform(translationX: 0, y: 0)
