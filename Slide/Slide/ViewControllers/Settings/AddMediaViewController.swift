149b21d60e57d3752c8f392ae21c1262c7df2ccf
Jennifer Kim <jenkim@bu.edu>
2019-07-03 11:19:36 -0400

    Added phone number textfield to Add Media
    To do: saving and verifying phone number via Firebase.

---
+++ Slide/Slide/ViewControllers/Settings/AddMediaViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AddMediaViewController.swift
+//  Slide
+//
+//  Created by Jennifer Kim on 7/3/19.
+//  Copyright © 2019 Roodac. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseFirestore
+
+class AddMediaViewController: UIViewController {
+    
+    @IBOutlet weak var phoneNum: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+}
