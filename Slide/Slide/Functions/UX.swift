8affa741b8f30b43f6491b119501b01c1be12fe6
Valeriy Soltan <valeriy.soltan@gmail.com>
2019-08-06 14:08:20 -0400

    implemented abiltiy to hide keyboard with a tap or swipe

--- Slide/Slide/Functions/UX.swift
+++ Slide/Slide/Functions/UX.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import UIKit
+
+class UX {
+    
+}
+
+// allows the user to dismiss the keyboard once they are done editing
+extension UIViewController {
+    func hideKeyboardOnGesture() {
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        
+        let scrollUp = UISwipeGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        let scrollDown = UISwipeGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        scrollUp.direction = .up
+        scrollDown.direction = .down
+        
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+        view.addGestureRecognizer(scrollUp)
+        view.addGestureRecognizer(scrollDown)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
